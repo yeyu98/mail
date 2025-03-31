090d67aef0919347dc2047e3280e6491b728c5c9
yeyu98 <763525908@qq.com>
2025-03-31 22:37:17 +0800

    feat

--- app/worker.ts
+++ app/worker.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2025-03-31 20:16:43
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2025-03-31 22:36:58
+ * @Description: 
+ */
 import { drizzle } from 'drizzle-orm/d1';
 import { Customers } from './lib/schema';
 
@@ -5,13 +12,17 @@ import { Customers } from './lib/schema';
 //   DB: D1Database
 // }
 
-const handleEmail = async (message: any, env: any, ctx: any): Promise<void> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handleEmail = async (message: any): Promise<Response> => {
     const text = new Response(message.raw)
     console.log(text)
+    return new Response('Email received', {status: 200})
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async fetch(request: Request, env: any): Promise<Response> {
     try {
       const url = new URL(request.url)
       const db = drizzle(env.DB)
@@ -32,8 +43,9 @@ export default {
       return new Response(`Internal Server Error ${error}`, {status: 500})
     }
   },
-  async email(message: any, env: any, ctx: any): Promise<void> {
-    await handleEmail(message, env, ctx)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async email(message: any): Promise<void> {
+    await handleEmail(message)
   },
 }
   
